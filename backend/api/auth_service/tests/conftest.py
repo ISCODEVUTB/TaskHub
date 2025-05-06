47a83a50ddaa1f125bd4ec5637b637d32352481a
vollereiseelee <wcuelloh@gmail.com>
2025-05-05 23:23:18 -0500

    fixed conftest

--- backend/api/auth_service/tests/conftest.py
+++ backend/api/auth_service/tests/conftest.py
@@ -1,8 +1,8 @@
-import pytest
-from fastapi.testclient import TestClient
-from backend.api.Gateway.main import app  # This is your actual FastAPI app
+from backend.api.Gateway.main import app  # noqa: F401
 
+import sys
+from pathlib import Path
 
-@pytest.fixture
-def client():
-    return TestClient(app)
+# Dynamically add the project root to sys.path
+project_root = Path(__file__).resolve().parents[3]
+sys.path.append(str(project_root))
