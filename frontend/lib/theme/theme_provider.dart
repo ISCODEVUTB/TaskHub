5e09ae4c2648df82b3aef1f4f9f9fd4fc5bdaa06
vollereiseelee <wcuelloh@gmail.com>
2025-05-15 21:48:05 -0500

    Made Frontend. (Still subject to changes)

---
+++ frontend/lib/theme/theme_provider.dart
@@ -0,0 +1,33 @@
+
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class ThemeProvider extends ChangeNotifier {
+  static const _themeKey = 'isDarkMode';
+  bool _isDarkMode = false;
+
+  ThemeProvider() {
+    _loadThemePreference();
+  }
+
+  bool get isDarkMode => _isDarkMode;
+
+  ThemeMode get themeMode => _isDarkMode ? ThemeMode.dark : ThemeMode.light;
+
+  void toggleTheme() {
+    _isDarkMode = !_isDarkMode;
+    _saveThemePreference();
+    notifyListeners();
+  }
+
+  Future<void> _loadThemePreference() async {
+    final prefs = await SharedPreferences.getInstance();
+    _isDarkMode = prefs.getBool(_themeKey) ?? false;
+    notifyListeners();
+  }
+
+  Future<void> _saveThemePreference() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool(_themeKey, _isDarkMode);
+  }
+}
