f25f9fe6134dc32bddff91850df8e850b6a19123
Oyhs-co <oyhsotelo@gmail.com>
2025-06-08 17:57:20 -0500

    feat: Mejorar la gestión de la conexión a la base de datos y agregar manejo de errores

--- backend/api/shared/utils/db.py
+++ backend/api/shared/utils/db.py
@@ -4,15 +4,23 @@ from typing import Generator
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session, sessionmaker
+from sqlalchemy.pool import NullPool
 
 # Load environment variables
 load_dotenv()
 
 # Database URL
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
-
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db") 
+print(f"🔌 Intentando conectarse a la base de datos:\n{DATABASE_URL}")
 # Create database engine
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, poolclass=NullPool)
+
+try:
+    conn = engine.connect()
+    print("✅ Conexión exitosa")
+    conn.close()
+except Exception as e:
+    print("❌ Error:", str(e))
 
 # Create session local
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
